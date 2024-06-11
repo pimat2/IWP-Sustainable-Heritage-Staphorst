c15f14d7974ac908b3946efd61c45a410b926a3d
DanielBatalski <ii145862@gmail.com>
2024-06-11 18:13:56 +0200

    building for WebGL

--- active-ragdolls/Assets/Scripts/PlayerPressurePlate.cs
+++ active-ragdolls/Assets/Scripts/PlayerPressurePlate.cs
@@ -1,8 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.EditorTools;
 using UnityEngine;
-using UnityEngine.InputSystem;
+
 
 public class PlayerPressurePlate : MonoBehaviour
 {
--- active-ragdolls/Assets/Scripts/SecondPuzzle/SecondPuzzleChecker.cs
+++ active-ragdolls/Assets/Scripts/SecondPuzzle/SecondPuzzleChecker.cs
@@ -1,9 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.EditorTools;
 using UnityEngine;
-using UnityEngine.Timeline;
 
 public class SecondPuzzleChecker : MonoBehaviour
 {
